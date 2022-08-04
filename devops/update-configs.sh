59a812eb867ef8ff66f0638f5304d8758c33a08e
GZykov <georgy.zykov@firstlinesoftware.com>
2022-08-04 16:52:19 +0300

    JB: init project

---
+++ devops/update-configs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for dir in ../configs/*/     # list directories in the form "/tmp/dirname/"
+do
+    dir=${dir%*/}      # remove the trailing "/"
+    curl --request PUT --data-binary @backend/configuration/${dir##*/}/application.yaml http://localhost:8500/v1/kv/config/${dir##*/}/application.yaml
+done
